68136133585844454c188d765ee00d2e97ad8d84
tefnick <nickruiz210@gmail.com>
2024-04-16 18:41:35 -0500

    improved loading skeleton UI

--- app/loading.js
+++ app/loading.js
@@ -5,20 +5,18 @@ export default function Loading() {
         // <h2 className='flex-col justify-center min-h-screen bg-teal-400 font-sans text-center'>Loading posts...</h2>
         <main className='relative flex min-h-screen bg-teal-400 font-sans'>
             <section className='flex-col justify-center overflow-hidden py-6 px-6 mx-auto text-center'> 
-                <section className='p-7 mt-4 shadow-lg rounded-lg text-center'>
-                    <article className="relative shadow-md">
-                        <div className="space-y-6">
+                <section className='bg-slate-200 p-7 mt-4 shadow-lg rounded-lg text-center'>
+                    <article className="bg-slate-200 relative shadow-md mb-4">
+                        <div className="shadow-slate-300 space-y-6 space-x-4 p-4">
                             <div className="space-y-2">
-                                <Skeleton className="w-[30ch] h-[1.25rem]"/>
-                                <Skeleton className="w-[45ch] h-[1rem]"/>
+                                <Skeleton className="ml-4 w-[15ch] h-[1.25rem] mb-2 animate-pulse"/>
                             </div>
                             <div className="space-y-2">
-                                <Skeleton className="w-[30ch] h-[1.25rem]"/>
-                                <Skeleton className="w-[45ch] h-[1rem]"/>
+                                <Skeleton className="w-[20ch] h-[1.25rem] animate-pulse"/>
                             </div>
                             <div className="space-y-2">
-                                <Skeleton className="w-[30ch] h-[1.25rem]"/>
-                                <Skeleton className="w-[45ch] h-[1rem]"/>
+                                <Skeleton className=" h-[1rem] mb-6 animate-pulse"/>
+                                <Skeleton className="w-[30ch] h-[1rem] animate-pulse"/>
                             </div>
                         </div>
                     </article>
